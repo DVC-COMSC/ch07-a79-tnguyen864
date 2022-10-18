0ecc5271dc1c021798409103e668c9b186ac0639
Thuy Nguyen <thuicide@gmail.com>
2022-10-17 17:27:57 -0700

    Assign7-9

--- main.py
+++ main.py
@@ -7,12 +7,30 @@ numbers = [     [99, 11, 66, 86, 55],
 rnum = len(numbers)
 cnum = len(numbers[0])
 
+# Get sum of rows and print
 print ('Sum of rows: ', end=' ')
 for i in range(rnum):
 	rsum = sum(numbers[i])
 	print (rsum, end=' ')
 print()
 
-# ******************************
-# Make your Code
-# ******************************
+# Get sum of columns and print
+print ('Sum of columns: ', end = ' ')
+for i in range (cnum):
+    csum = 0
+    for j in range(rnum):
+        csum += numbers[j][i]
+    print(csum, end = ' ')
+print()
+
+# Find row index number that has greatest sum and print
+rowSum = []
+print ('The row that has greatest sum: ', end = ' ')
+for i in range(rnum):
+    rsum = sum(numbers[i])
+    rowSum.append(rsum)
+print(rowSum.index(max(rowSum)))
+
+# Find greatest number in list and print
+maxOfList = max(max(numbers))
+print ('The greatest number: ', maxOfList)
